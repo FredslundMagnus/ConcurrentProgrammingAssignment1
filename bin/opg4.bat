af3f29457b31cc7d4b481b557776aaa7bc76996a
Magnus Fredslund <fredslundmagnus@gmail.com>
2021-09-12 08:41:15 +0200

    Fix

--- bin/opg4.bat
+++ bin/opg4.bat
@@ -1,1 +1,1 @@
-FOR %%A IN (1,2,4,8,16) DO FOR %%B IN (10,100,1000) DO java Search -W 10 -R 100 Chromosome.txt A %%B %%A >> ../data/Problem4/threads-%%A-batch-%%B.txt
+FOR %%A IN (1,2,4,8,16) DO FOR %%B IN (100) DO java Search -W 10 -R 100 Chromosome.txt A %%B %%A >> ../data/Problem4/threads-%%A-batch-%%B.txt
